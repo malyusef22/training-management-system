866f18f9f4a7933e2af77ed9d0269900422aea9b
Maram Alyusef <malyusef@ejada.com>
2026-07-28 15:38:29 +0300

    test

--- src/main/java/com/maram/training/TrainingManagementApplication.java
+++ src/main/java/com/maram/training/TrainingManagementApplication.java
@@ -1,3 +1,4 @@
+
 package com.maram.training;
 import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication public class TrainingManagementApplication { public static void main(String[] a){SpringApplication.run(TrainingManagementApplication.class,a);} }
